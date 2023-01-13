56aebaf880e79e2e48e37cfd2bb9ed476ce3d05d
awal umar <awalumar277@gmail.com>
2023-01-13 08:58:58 -0800

    unfinish

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    while number <= 100:
+        if number/3 == 0:
+	    print("Fizz")
+	elif number/5 == 0:
+	    print("Buzz")
+	elif number/3==0 and number/5==0:
+	    print("FizzBuzz")
+    return number
