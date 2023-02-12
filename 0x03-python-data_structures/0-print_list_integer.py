afb34067375f1c2225f86ce7dcb9f3743f85b543
awal umar <awalumar277@gmail.com>
2023-02-12 03:44:55 -0800

    list of integer

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+
+   """
+	Print a list of element
+
+	Arguments:
+		my_list: the list of integers
+
+	Returns:
+		None
+   """
+   for element in my_list:
+   	print("{:d}".format(element))
