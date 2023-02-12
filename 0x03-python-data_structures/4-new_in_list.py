21726ba98da666d4d2a7dca94ad3f2097b50d7f6
awal umar <awalumar277@gmail.com>
2023-02-12 07:00:06 -0800

    not complete et

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+
+   """Description
+   Write a function that replaces an element in a list at a specific
+   position without modifying the original list (like in C).
+
+    Parameters:
+    		my_list: the list of element
+		idx: the position of the element
+		element: the item in the list
+	Return
+	a modified list
+   """
+   new_list = my_list[:]
+   new_list[idx] = element
+
+   if (my_list[idx] - 1) > my_list[idx] < 0:
+   	return my_list
+   return my_list
