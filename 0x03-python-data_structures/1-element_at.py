bb99778ad13714cbb91531097784788be50ca111
awal umar <awalumar277@gmail.com>
2023-02-12 04:14:15 -0800

    index

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+
+   """Description
+   function that retrieves an element from a list like in C
+
+      Arguments:
+      		mu_list: the list of element we are require to use
+		idx: index or position of the element
+   """
+   if (my_list[idx] - 1) > my_list[idx] < 0:
+   	None
+   else:
+   	return my_list[idx]
