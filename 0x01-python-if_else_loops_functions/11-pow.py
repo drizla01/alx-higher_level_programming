bbbf81353fc71a7131d869f265872d0b9f807a67
Awal Umar <awalumar277@gmail.com>
2023-08-14 11:58:06 -0700

    a ^ b

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+def pow(a, b):
+    """Power of two argument
 
+    Args:
+        a: base argument
+        b: indice arguement
+
+    Return:
+        value: a ^ b
+    """
+    return (a ^ b)
