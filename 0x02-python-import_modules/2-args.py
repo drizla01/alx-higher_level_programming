bb2109e04b4ee36b384107eef3ec97be6f569edb
Awal Umar <awalumar277@gmail.com>
2023-08-14 04:41:06 -0700

    indentation fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,13 +4,13 @@ if __name__ == "__main__":
 
     argv_len = len(sys.argv) - 1
     if (len(sys.argv) < 2):
-       print("{:d} arguments:".format(argv_len))
+        print("{:d} arguments:".format(argv_len))
     else:
         i = 1
         if (argv_len == 1):
-           print("{:d} arguments:".format(argv_len))
+            print("{:d} arguments:".format(argv_len))
         else:
-           print("{:d} arguments:".format(argv_len))
+            print("{:d} arguments:".format(argv_len))
         for arg in sys.argv[1:]:
             print("{:d}: {}".format(i, arg))
             i += 1
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-   import sys
+    import sys
 
-   sum = 0
-   if (len(sys.argv) < 2):
-      print("{:d}".format(0))
-   else:
-      for arg in sys.argv[1:]:
-          sum += int(arg)
-      print("{:d}".format(sum))
+    sum = 0
+    if (len(sys.argv) < 2):
+        print("{:d}".format(0))
+    else:
+        for arg in sys.argv[1:]:
+            sum += int(arg)
+        print("{:d}".format(sum))
