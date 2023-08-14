ac8ce7f622c9a6933288bd612ea386acc92b74d1
Awal Umar <awalumar277@gmail.com>
2023-08-14 16:32:43 -0700

    12. Fizz Buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-
+    for fb in range(1, 101):
+        if fb % 3 == 0 and fb % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif fb % 5 == 0:
+            print("Buzz", end=" ")
+        elif fb % 3 == 0:
+            print("Fizz", end=" ")
+        else:
+            print(fb, end=" ")
