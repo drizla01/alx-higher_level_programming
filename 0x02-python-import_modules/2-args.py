0960a95497e677991b227264f1bbdd701783a122
Awal Umar <awalumar277@gmail.com>
2023-08-14 04:46:46 -0700

    full stopgit add . not :

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
 
     argv_len = len(sys.argv) - 1
     if (len(sys.argv) < 2):
-        print("{:d} arguments:".format(argv_len))
+        print("{:d} arguments.".format(argv_len))
     else:
         i = 1
         if (argv_len == 1):
