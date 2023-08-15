b7cc9d203caf6dbaf711cd999af1055f0e2fe632
Awal Umar <awalumar277@gmail.com>
2023-08-15 01:17:16 -0700

    fix this logic

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 cue = abs(number) % 10
-sign = "" if number > 0 else "-"
-if cue > 5:
-    print("Last digit of {} is {}{} and is greater than 5 and not \
-0".format(number, sign, cue))
-elif cue < 6 and cue != 0:
-    print("Last digit of {} is {}{} and is less than 6 and not \
-0".format(number, sign, cue))
+if cue > 5 and number > 0:
+    print("Last digit of {} is {} and is greater than 5".format(number, cue))
+elif cue < 6 and number > 0 and cue != 0:
+    print("Last digit of {} is {} and is less than 6 and not \
+0".format(number, cue))
+elif cue < 6 and number < 0:
+    print("Last digit of {} is -{} and is less than 6 and not \
+0".format(number, cue))
 else:
     print("Last digit of {} is {} and is 0".format(number, cue))
