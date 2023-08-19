51fdba409655844e1983d030bfba73fb0d61936e
Awal Umar <awalumar277@gmail.com>
2023-08-19 11:35:10 -0700

    4. Only differents

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-  3     """
-  4     Return the set of common element present in only one set
-  5 
-  6     Parameter:
-  7         set_1 : first set of element
-  8         set_2 : second set of element
-  9 
- 10     Return:
- 11         the result of the element present in only one sets
- 12     """
- 13 
- 14     return (set_1 ^ set_2)
+    """
+    Return the set of common element present in only one set
 
+    Parameter:
+          set_1 : first set of element
+          set_2 : second set of element
+
+    Return:
+         the result of the element present in only one sets
+    """
+
+    return (set_1 ^ set_2)
