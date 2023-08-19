8e951460c5fb3435f63e795a44f32c17ef0559c7
Awal Umar <awalumar277@gmail.com>
2023-08-19 11:39:41 -0700

    5. Number of Keys

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """
+    A function that returns the number of keys in a dictionary
+
+    Parameters:
+        a dictionary with key, value pair
+
+    Return:
+        the len of the key
+    """
+
+    return(len(a_dictionary))
