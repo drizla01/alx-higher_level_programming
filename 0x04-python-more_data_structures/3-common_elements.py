23d29edea79327403ee27d5d1577cc5532960122
Awal Umar <awalumar277@gmail.com>
2023-08-19 11:25:38 -0700

    3. Present in both

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    """
+    Return the set of common element in two set
+
+    Parameter:
+        set_1 : first set of element
+        set_2 : second set of element
+
+    Return:
+        the result of the element common in two sets
+    """
+
+    return (set_1 & set_2)
