07ae23d32833d30f1dbf10171a7373dc7126f6fe
Awal Umar <awalumar277@gmail.com>
2023-08-24 05:55:59 -0700

    5. 00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for num in range(0, 100):
-    if num <= 9:
-        print("0{},".format(num), end=" ")
-    elif num >= 10 and num < 99:
-        print("{},".format(num), end=" ")
-    elif num == 99:
+    if num == 99:
         print("{}".format(num))
+    else:
+        print("{:0>2}".format(num), end=", ")
