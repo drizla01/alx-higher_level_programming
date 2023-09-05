d80f0c7304d58ad8b367c2459eda4e69f8ff5e5b
Awal Umar <awalumar277@gmail.com>
2023-09-05 01:31:57 -0700

    matrix divided test

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,25 +3,48 @@
 A function that divide all elements of a matrix
 """
 def matrix_divided(matrix, div):
-    outer_matrix = []
-    size = len(matrix[0])
-    try:
-        if not isinstance(matrix, list([int, float])):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-        elif not isinstance(div, type(int), type(float)):
-           raise TypeError("div must be a number")
-        elif div == 0:
-           raise ZeroDivisionError("division by zero")
+    """ divide all elements of a matrix
 
+    Args:
+        matrix: a list of list containing integers or floats
+        div: the number use as a divisor
+
+    Return:
+        list: a new matrix containing the result after using div
+    """
+    result = []
+    size = [0, 0]
+    message = (
+        "matrix must be a matrix (list of lists) of integers/floats",
+        "Each row of the matrix must have the same size",
+        "div must be a number",
+        "division by zero"
+    )
+
+    if not isinstance(matrix, list):
+        raise TypeError(message[0])
+    size[0] = len(matrix)
+    if size[0] == 0:
+        raise TypeError(message[0])
     for row in matrix:
-        inner_list = []
-        if not instance(row, type(row), list()):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-        if size != len(row):
-            raise TypeError("Each row of the matrix must have the same size")
-        for i in row:
-            if not isinstance(i, type(int), type(float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-            inner_list.append(round(num / div, 2))
-        outer_matrix.append(inner_list)
-    return (outer_matrix)
+        if not isinstance(row, list):
+            raise TypeError(message[0])
+        elif len(row) == 0:
+            raise TypeError(message[0])
+        else:
+            if size[1] == 0:
+                size[1] == len(row)
+            elif len(row) != size[1]:
+                raise TypeError(message[1])
+            for col in row:
+                if not isinstance(col, (int, float)):
+                    raise TypeError(message[0])
+    if not isinstance(div, (int, float)):
+        raise TypeError(message[2])
+    elif div == 0:
+        raise ZeroDivisionError(message[3])
+    else:
+        for row in matrix:
+            res_row = list(map(lambda x: round(x / div, 2), row))
+            result.append(res_row)
+        return (result)
