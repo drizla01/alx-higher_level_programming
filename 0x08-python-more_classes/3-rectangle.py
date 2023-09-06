4ac184b4de9c8bf005fbcc978b511a560b095a62
Awal Umar <awalumar277@gmail.com>
2023-09-06 07:40:25 -0700

    string representation

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,19 +1,42 @@
 #!/usr/bin/python3
+"""Modules
+"""
+
+
 class Rectangle:
+    """Represent 2D rectangle
+    """
     def __init__(self, width=0, height=0):
+        """Initialization
+
+        Args:
+            width and height both integer
+        """
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """Retrive the width of the rectangle
+
+        Returns:
+            int: width of the rectangle
+        """
         return self.__width
 
     @property
     def height(self):
+        """Retrive the height of the rectangle
+
+        Return:
+            int: height of the rectangle
+        """
         return self.__height
 
     @width.setter
     def width(self, value):
+        """Update the width
+        """
         self.__width = value
         try:
             if isinstance(value, int):
@@ -26,6 +49,8 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        """Update the width
+        """
         self.__height = value
         try:
             if isinstance(value, int):
@@ -37,15 +62,21 @@ class Rectangle:
         return
 
     def area(self):
+        """Return the area of the rectangle
+        """
         return self.__width * self.__height
 
     def perimeter(self):
+        """Return the perimater of the rectangle
+        """
         if self.__width == 0 or self.__height == 0:
             return 0
         else:
             return self.__width*2 + self.__height*2
 
     def __str__(self):
+        """Return the string  rep. of the width and height of the rectangle
+        """
         if self.__width == 0 or self.height == 0:
             return ''
         else:
