29e19cceba3152bb325a772e83353db5cd847f3c
Awal Umar <awalumar277@gmail.com>
2023-09-17 10:22:35 +0100

    student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Define a class student
+"""
+
+
+class Student:
+    """Represent a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialization of variables with public instance attribute
+        of firstname, lastname and age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """to retrieve a dictionary representation of a student"""
+        if type(attrs) == list and all(type(element) == str for element in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
