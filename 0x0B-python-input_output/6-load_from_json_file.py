4f0ab32715eeea7396bfaecf04faa61a2101c715
Awal Umar <awalumar277@gmail.com>
2023-09-17 10:08:34 +0100

    load json

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 """Module import here
 """
-from json import JSONDecoder
+import json
 
 
 def load_from_json_file(filename):
     """
     A function that create an Object from a "JSON file"
     """
-    lines = []
-    with open(filename, encoding="utf-8", mode="w") as file:
-        for line in file.readlines():
-            lines.append(line)
-
-    return JSONDecoder().decode("".join(lines))
+    with open(filename) as file:
+        return json.load(file)
---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Import Module
+"""
+
+
+def add_item():
