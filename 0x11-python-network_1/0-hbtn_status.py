db6e46bc6705779d7973b42bac19bbb2d047f96c
Awal Umar <awalumar277@gmail.com>
2024-03-03 11:15:17 -0800

    0. What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""Fetches https://alx-intranet.hbtn.io/status"""
-import urllib.request
+"""
+Fetches https://alx-intranet.hbtn.io/status
+"""
 
 if __name__ == "__main__":
+    import urllib.request
+
     request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen(request) as response:
         body = response.read()
