7501f650c16391deb22043fa1ca1c0c8e216954d
Awal Umar <awalumar277@gmail.com>
2024-03-17 14:00:29 +0000

    full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -8,6 +8,6 @@ CREATE TABLE IF NOT EXISTS second_table(
 
 INSERT INTO second_table(id, name, score)
 	values (1, 'John', 10),
-		(2, 'Alex', 3), 
+		(2, 'Alex', 3),
 		(3, 'Bob', 14),
 		(4, 'George', 8);
