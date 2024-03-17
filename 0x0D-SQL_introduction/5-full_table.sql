5dcb329594d673096d4d0d38de8c6b387cc4b2a5
Awal Umar <awalumar277@gmail.com>
2024-03-17 14:39:38 +0000

    fulltable

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 --write a scripts that prints the full description of the table
 -- first_table from the database hbtn_0c_0 in your MySQL server
-SHOW CREATE TABLE first_table;
+SHOW CREATE TABLE hbtn_0c_0.first_table;
