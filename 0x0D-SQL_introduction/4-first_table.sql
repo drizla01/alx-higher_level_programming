5df93c1e52ced0874807f3756ddf5b82ca85b08e
Awal Umar <awalumar277@gmail.com>
2024-03-17 13:43:31 +0000

    first table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- write a script that create a table called first_table
+-- in the current database in your MySQL server.
+CREATE TABLE IF NOT EXISTS FIRST_TABLE(
+    ID INTEGER,
+    NAME VARCHAR(256)
+);
