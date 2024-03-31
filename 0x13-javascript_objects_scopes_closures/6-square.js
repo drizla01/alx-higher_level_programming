42f7ebcf05ebba2a23d55a4fec19f48f18f9c066
Awal Umar <awalumar277@gmail.com>
2024-03-31 13:49:10 +0100

    6 Square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const PrevSquare = require('./5-square');
+
+class Square extends PrevSquare {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (typeof c === 'undefined') {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += c;
+      }
+      console.log(row);
+    }
+  }
+}
+module.exports = Square;
